2a18f55f8659c01595a3d17e0eeb7eba107dc934
Mario Gomes <mario.mgomes1@gmail.com>
2021-10-04 23:54:57 -0400

    update

--- Code/Mario/python/lab8.py
+++ Code/Mario/python/lab8.py
@@ -3,27 +3,38 @@ import random
 
 def init():
     total = 0
+    expenses = 0
+    loop_itiration = 100000
+    ticket_cost = 2
+    ROI = 0
 
-    for _ in range(100000):
+    for _ in range(loop_itiration):
         winning_numbers = pick6()
         ticket_numers = pick6()
         total_match = num_matches(
             winning=winning_numbers, ticket=ticket_numers)
         winning_amount = payout(total_match)
-        total += winning_amount - 2
+        total += winning_amount
+        expenses += ticket_cost
+        # print(winning_numbers, ticket_numers)
+        # winning = [1, 2, 4, 5, 6]
+        # ticket = [1, 2, 3, 5, 6]
+        # total_match = num_matches(winning, ticket)
+        # winning_amount = payout(total_match)
+    earnings = total - expenses
 
-    if (total < 0):
-        total = '{:,.2f}'.format(int(abs(total)))
-        print(total)
-        print(f"You're down ${total}")
+    if (earnings < 0):
+        m = '{:,.2f}'.format(int(abs(earnings)))
+        print(f"You're down ${m}")
     else:
-        print(f"You have won a total of {abs(total)}")
-        print(abs(-10))
+        print(f"You have won a total of {earnings}")
+    ROI = (earnings - expenses)/expenses * 100
+    print(f"Your total ROI is {round(ROI)}%")
 
 
 def pick6():
     random_six = []
-    for num in range(6):
+    for _ in range(6):
         random_six.append(random.randint(1, 99))
     return random_six
 
