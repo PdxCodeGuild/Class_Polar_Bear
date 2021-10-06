aca9b2cc34b6f3916db27d1e5afc4090ea33c5b5
Mario Gomes <mario.mgomes1@gmail.com>
2021-10-05 23:42:45 -0400

    created a function and changed the way list is generated

--- Code/Mario/python/lab9.py
+++ Code/Mario/python/lab9.py
@@ -1,5 +1,5 @@
 
-def one_or_eleven(card):
+def one_or_eleven(card, total):
     if total + 11 > 21 and type(card) == list:
         return 1
     elif total + 11 <= 21 and type(card) == list:
@@ -19,38 +19,34 @@ def hints(total):
         return "Already Busted"
 
 
-cards = {
-    "1": 1,
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "10": 10,
-    "a": [1, 11]
-}
+def start_game():
+    cards = {
+        "a": [1, 11]
+    }
+    for num in range(1, 11):
+        cards[str(num)] = num
+    total = 0
+    print(cards)
 
-total = 0
+    options = ["first", "second", "third"]
+    user_cards = []
+    play_again = True
 
-options = ["first", "second", "third"]
-user_cards = []
-play_again = True
+    for option in range(len(options)):
+        try:
+            user_input = input(f"What is your {options[option]} card?").lower()
+            if user_input in cards.keys():
+                if user_input == 'a':
+                    total += one_or_eleven(cards[user_input], total)
+                elif user_input == 'j' or user_input == 'q' or user_input == 'k':
+                    user_input = "10"
+                else:
+                    total += (cards[user_input])
+                    user_cards.append(cards[user_input])
+        except:
+            user_cards.append(cards[user_input])
+    advise = hints(total)
+    print(f"{total} {advise}")
 
-for option in range(len(options)):
-    try:
-        user_input = input(f"What is your {options[option]} card?").lower()
-        if user_input == 'j' or user_input == 'q' or user_input == 'k':
-            user_input = "10"
-        if user_input in cards.keys():
-            if user_input == 'a':
-                total += one_or_eleven(cards[user_input])
-            else:
-                total += (cards[user_input])
-                user_cards.append(cards[user_input])
-    except:
-        user_cards.append(cards[user_input])
-advise = hints(total)
-print(f"{total} {advise}")
+
+start_game()
