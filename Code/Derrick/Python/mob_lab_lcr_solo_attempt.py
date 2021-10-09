66b4208a2653c8533f1ea34ce6cdc2af1dcffbfe
dj51234 <dj51234@yahoo.com>
2021-10-09 02:33:01 -0400

    completed solo attempt at LCR mob lab for practice

--- Code/Derrick/Python/mob_lab_lcr_solo_attempt.py
+++ Code/Derrick/Python/mob_lab_lcr_solo_attempt.py
@@ -1,18 +1,15 @@
-
-# Each player begins with 3 chips. Players take turns rolling three six-sided dice, each of which is marked with "L", "C", "R" on one side, and a single dot on the three remaining sides. For each "L" or "R" thrown, the player must pass one chip to the player to their left or right, respectively. A "C" indicates a chip to the center (pot). A dot has no effect.
-
-# If a player has fewer than three chips left, they're still in the game but their number of chips is the number of dice they roll on their turn, rather than rolling all three. When a player has zero chips, they pass the dice on their turn, but may receive chips from others and take their next turn accordingly. The winner is the last player with chips left, and wins the center pot.
-
-# When the program starts, it should ask for the names of the players, until the user enters 'done'. Then it should run the simulation, tell the user who won, and ask the player whether they'd like to play again. We can represent the players as a list of dictionaries with each dictionary containing two key-value pairs: player's name and the number of chips they have, e.g. {'name': 'Billy', 'chips': 3}.
 import random
 
 players = []
+chipCounts = []
 pot = 0
 dice = ['L','C','R',1,1,1]
 
+# add players objects to players list
 def addPlayer(name,chipCount):
     players.append({'name': name,'chips': chipCount})
 
+# roll dice and create temporary list 
 def rollDice(n):
     outcome = []
 
@@ -21,6 +18,7 @@ def rollDice(n):
 
     return outcome
 
+# checks temporary list for L C or R and performs the appropriate actions
 def diceCheck():
     if 'L' in x:
         players[i]['chips'] -= 1
@@ -39,9 +37,9 @@ def diceCheck():
 
         else:
             players[i + 1]['chips'] += 1
-    
-        
 
+
+# ask user for player names
 while True:
     name = input('Please enter player name or \'done\' to continue to the game: ')
 
@@ -49,12 +47,12 @@ while True:
         break
 
     addPlayer(name,3)
-       
 
-chipCounts = []
 
+# create initial chip count 
 for i in range(len(players)):
     chipCounts.append(players[i]['chips'])
+
 
 while chipCounts.count(0) != len(chipCounts) - 1:
     
@@ -68,10 +66,11 @@ while chipCounts.count(0) != len(chipCounts) - 1:
             x = rollDice(players[i]['chips'])
             diceCheck()
             
-
+    # update chipCount list 
     for i in range(len(chipCounts)):
         chipCounts[i] = players[i]['chips']
-            
+
+    # to compare chip counts and verify if only 1 player still has chips        
     if chipCounts.count(0) == len(chipCounts) - 1:
         for i in range(len(players)):
             if players[i]['chips'] > 0:
