498d10001b5ff67d8abcbfc78e67583ec6c97461
dj51234 <dj51234@yahoo.com>
2021-10-13 22:10:21 -0400

    finished first part of rain data lab

--- Code/Derrick/Python/rain_data_lab/rain_data.py
+++ Code/Derrick/Python/rain_data_lab/rain_data.py
@@ -1,33 +1,32 @@
-from datetime import datetime 
-
-
-date = datetime.strptime('25-MAR-2016', '%d-%b-%Y')
+### 1 = .01 inches of rain ###
 
 rain_dict = {}
-
-with open('post_office_rain.txt') as file:
+rain_chart = 'post_office_rain.txt'
+with open(rain_chart) as file:
     plain_text = file.read()
     plain_text = plain_text.replace('\n',' ')
 
-
-        
-    
-
-    new_list = plain_text.split(' ')
-    
-    new_list_obj = filter(lambda x: x != "", new_list)
-
-    new_list = list(new_list_obj)
+    new_list = plain_text.split()
 
 for i in range(len(new_list)):
-    if len(new_list[i]) == 11:
-        rain_dict[new_list[i]] = int(new_list[i+1].rstrip())          
 
+    if len(new_list[i]) == 11:
+        try:
+            rain_dict[new_list[i]] = int(new_list[i+1])
+        except ValueError:
+            continue 
+                
 # Calculate mean
 sum = 0
-# for value in rain_dict.values():
-#     print(value)
+for value in rain_dict.values():
+    sum += value
+    mean = round(sum / len(rain_dict.values()),2)
 
+earliest_date = list(rain_dict.keys())[-1]
+location = ''
+for i in rain_chart:
+    location += i
+    if i == '.':
+        break
 
-print(rain_dict)
-# print(date)
+print(f'Starting from {earliest_date} the daily average rainfall for {location} is {round(mean * .01,2)} inches')
