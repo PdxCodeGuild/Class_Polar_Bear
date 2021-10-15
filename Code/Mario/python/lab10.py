e36b554295c626adf1ab781cb25a38bafe26effa
Mario Gomes <mario.mgomes1@gmail.com>
2021-10-14 23:35:14 -0400

    lab10

---
+++ Code/Mario/python/lab10.py
@@ -0,0 +1,32 @@
+import requests
+import os
+import time
+
+
+def clear(): return os.system('cls')
+
+
+headers = {
+    'Accept': 'application/json',
+    'term': 'dog',
+    'limit': '1'
+}
+
+search = input("Enter a search term: ")
+see_more = True
+
+response = requests .get(f"https://icanhazdadjoke.com/search?term={search}",
+                         headers=headers, params="dog").json()['results']
+# print(response)
+for i in range(len(response)):
+    joke = response[i]['joke']
+    clear()
+    if see_more:
+        time.sleep(.3)
+        print(f"{joke}\n")
+        time.sleep(3)
+        show_another_joke = input(
+            f"Would you like to see another one?\ny/n ? ")
+        see_more = True if show_another_joke == 'y' else False
+    else:
+        break
