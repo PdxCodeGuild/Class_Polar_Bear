25e8d9cbda9aeec749f668a011ed5051667411a6
Ken Mazur <mazurkr@gmail.com>
2021-10-14 19:40:43 -0700

    Dad Joke

---
+++ Code/Ken/Python/Dad_Joke.py
@@ -0,0 +1,14 @@
+import requests
+
+url = 'https://icanhazdadjoke.com/'
+
+headers = {
+    'Accept': 'application/json'
+}
+
+response = requests.get(url, headers=headers)
+
+data = response.json()
+
+print(data['joke'])
+
