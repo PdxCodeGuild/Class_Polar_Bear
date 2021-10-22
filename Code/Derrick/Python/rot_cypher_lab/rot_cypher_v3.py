56e76fe882fc0b7268ab2f05e8179a95650c3211
dj51234 <dj51234@yahoo.com>
2021-10-22 15:20:54 -0400

    rot cipher code cleanup 1-3

--- Code/Derrick/Python/rot_cypher_lab/rot_cypher_v3.py
+++ Code/Derrick/Python/rot_cypher_lab/rot_cypher_v3.py
@@ -13,10 +13,10 @@ class RotCipher:
 
     def create_rot_alphabet(self):
         for i in range(len(self.alpha)):
-            if self.alpha.index(self.alpha[i]) < self.rot_amount and i + self.rot_amount <= 25:
+            if self.alpha.index(self.alpha[i]) < self.rot_amount and i + self.rot_amount <= len(self.alpha):
                 new_letter = self.alpha[i + self.rot_amount]
 
-            elif i + self.rot_amount > 25:
+            elif i + self.rot_amount > len(self.alpha):
                 remainder = len(self.alpha) - self.alpha.index(self.alpha[i]) # q = 16, 25 - 16 = 9, 15-9 = correct letters index
                 new_index = self.rot_amount - remainder
                 new_letter = self.alpha[new_index]
