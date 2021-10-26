c4b6daaae24a2ce7aca25c7d79af9b63a6aa650a
Arthur Andama <a_andama@yahoo.com>
2021-10-25 18:17:35 -0700

    updated

--- Code/Arthur/Python_Labs/AuReIn.py
+++ Code/Arthur/Python_Labs/AuReIn.py
@@ -52,32 +52,117 @@ This corresponds to a 11th Grade level of difficulty
 that is suitable for an average person 16-17 years old.
 --------------------------------------------------------
 '''
-# use anybook and use the same policy as previous
+# # use anybook and use the same policy as previous
 
+# import string
+# import math
+
+
+
+# with open('C:/Users/REACT/Desktop/pdx_code/New_Polar/Code/Arthur/Python_Labs/rider.txt','r',encoding='utf-8') as file:
+#     text = file.read()
+
+# # print(text[:1000])
+# text = text.lower()
+# #remove punctuations
+# purge = string.punctuation + string.digits + '\n\n'
+
+# sentence =0
+# characters = 0
+# for char in purge:
+#     if char in text:
+#         text = text.replace(char,"")
+#         sentence +=1
+#     elif char in string.ascii_letters:
+#         characters += 1
+# # text = text.replace(" ","")
+# text = text.split()
+# # print(text[:100])
+
+# ari = (4.71 * (char/text) + 0.5 * (text/sentence) - 21.43)
+# ari = math.ceil(ari)
+# if ari > 14:
+#     ari = 14
+# ari_scale = {
+#      1: {'ages':   '5-6', 'grade_level': 'Kindergarten'},
+#      2: {'ages':   '6-7', 'grade_level':    '1st Grade'},
+#      3: {'ages':   '7-8', 'grade_level':    '2nd Grade'},
+#      4: {'ages':   '8-9', 'grade_level':    '3rd Grade'},
+#      5: {'ages':  '9-10', 'grade_level':    '4th Grade'},
+#      6: {'ages': '10-11', 'grade_level':    '5th Grade'},
+#      7: {'ages': '11-12', 'grade_level':    '6th Grade'},
+#      8: {'ages': '12-13', 'grade_level':    '7th Grade'},
+#      9: {'ages': '13-14', 'grade_level':    '8th Grade'},
+#     10: {'ages': '14-15', 'grade_level':    '9th Grade'},
+#     11: {'ages': '15-16', 'grade_level':   '10th Grade'},
+#     12: {'ages': '16-17', 'grade_level':   '11th Grade'},
+#     13: {'ages': '17-18', 'grade_level':   '12th Grade'},
+#     14: {'ages': '18-22', 'grade_level':      'College'}
+# }
+
+# message = f'''
+# --------------------------------------------------------
+# The ARI for The Rider is  {ari}
+# This corresponds to a {ari_scale[ari]['grade_level']} level of difficulty
+# that is suitable for an average person {ari_scale[ari]['ages']} years old.
+# --------------------------------------------------------
+# '''
+# print(message)
+
+
+#modified code
 import string
+import math
 
-ari_scale = {
 
-}
-with open('rider.txt','r',encoding='utf-8') as file:
+
+with open('C:/Users/REACT/Desktop/pdx_code/New_Polar/Code/Arthur/Python_Labs/rider.txt','r',encoding='utf-8') as file:
     text = file.read()
 
-# print(text[:1000])
-text = text.lower()
-#remove punctuations
-purge = string.punctuation + string.digits + '\n\n'
+sentences = 0
+characters = 0
+text = text.replace('..', '.')
+for char in text:
+    if char in '.!?':
+        sentences += 1
+    elif char in string.ascii_letters:
+        characters += 1
 
-for char in purge:
-    if char in text:
-        text = text.replace(char,"")
-# text = text.replace(" ","")
-text = text.split(' ')
-print(text[:100])
+words = len(text.split())
+
+# print(sentences, words, characters)
+ari = 4.71 * (characters / words) + 0.5 * (words / sentences) - 21.43
+ari = math.ceil(ari)
+if ari > 14:
+    ari = 14
+
+ari_scale = {
+     1: {'ages':   '5-6', 'grade_level': 'Kindergarten'},
+     2: {'ages':   '6-7', 'grade_level':    '1st Grade'},
+     3: {'ages':   '7-8', 'grade_level':    '2nd Grade'},
+     4: {'ages':   '8-9', 'grade_level':    '3rd Grade'},
+     5: {'ages':  '9-10', 'grade_level':    '4th Grade'},
+     6: {'ages': '10-11', 'grade_level':    '5th Grade'},
+     7: {'ages': '11-12', 'grade_level':    '6th Grade'},
+     8: {'ages': '12-13', 'grade_level':    '7th Grade'},
+     9: {'ages': '13-14', 'grade_level':    '8th Grade'},
+    10: {'ages': '14-15', 'grade_level':    '9th Grade'},
+    11: {'ages': '15-16', 'grade_level':   '10th Grade'},
+    12: {'ages': '16-17', 'grade_level':   '11th Grade'},
+    13: {'ages': '17-18', 'grade_level':   '12th Grade'},
+    14: {'ages': '18-22', 'grade_level':      'College'}
+}
+
+
+message = f'''
+--------------------------------------------------------
+The ARI for Rider is is {ari}
+This corresponds to a {ari_scale[ari]['grade_level']} level of difficulty
+that is suitable for an average person {ari_scale[ari]['ages']} years old.
+--------------------------------------------------------
+'''
+
+print(message)
 
 
 
-
-
-# with open('rider.txt', 'r', encoding='utf-8') as file:
-#     text = file.read()
-# print(text)
