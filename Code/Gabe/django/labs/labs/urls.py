a6ad95cf9f3bed5e20275f8df142726c9551abf2
gabinochacon8 <gabino.chacon8@gmail.com>
2021-10-27 10:11:18 -0700

    made labs django project with routing app and copied all html/css file into app templates. Made a text file with notes for building django projects/apps

--- Code/Gabe/django/labs/labs/urls.py
+++ Code/Gabe/django/labs/labs/urls.py
@@ -1,4 +1,4 @@
-"""firstproject URL Configuration
+"""labs URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include('hello.urls')),
-    path('newyears/', include('newyears.urls')),
-    path('guess/', include('guess_num.urls')),
+    path('routing/', include('routing.urls')),
 ]
