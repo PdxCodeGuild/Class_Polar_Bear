1c777e4e8d04b6ca65145d396098b925ef8c7dbd
Anthony Wallace <anthony@Anthonys-MBP.lan>
2021-11-02 09:43:50 -0700

    Updated .gitignore | message board demo

--- 3 Django/solutions/messageboard/board/urls.py
+++ 3 Django/solutions/messageboard/board/urls.py
@@ -1,3 +1,8 @@
+from django.urls import path
 
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('messageboard/', views.home_page, name="home_page"),
+    path('new_message/', views.new_message, name="new_message")
+]
