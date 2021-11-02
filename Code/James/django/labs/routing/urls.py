881652fc55b5acfdc93741fd0cf952b25a59aaa7
jamest7 <james.ed.thornton@gmail.com>
2021-11-01 20:54:37 -0500

    corrected bio

--- Code/James/django/labs/routing/urls.py
+++ Code/James/django/labs/routing/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('routing', views.routing, name='routing'),
+    path('routing/', views.routing, name='routing'),
     path('animations/', views.animations, name='animations'),
     path('company/', views.company, name='company'),
     path('blog/', views.blog, name='blog'),
