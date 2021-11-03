5ca949718fdc8a4d086b95f639a2924b6577d385
gabinochacon8 <gabino.chacon8@gmail.com>
2021-11-02 18:07:11 -0700

    created message board

---
+++ Code/Gabe/django/message_board/board/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Message(models.Model):
+  text_content = models.CharField(max_length=240)
+  user = models.CharField(max_length=40)
+  date = models.DateField(auto_created=True)
+
+def __str__(self):
+  return f'{self.date} -- {self.text_content}, {self.user}'
