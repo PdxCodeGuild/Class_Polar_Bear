fe1572548abaf06dc4b3fdc2f46b7191a1cc76b3
Ken Mazur <mazurkr@gmail.com>
2021-11-03 19:54:44 -0700

    Polls

--- Code/Ken/DJango/Labs/Labs/urls.py
+++ Code/Ken/DJango/Labs/Labs/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('routing/', include('routing.urls')),
-    path('rpsapp/', include('rpsapp.urls'))
+    path('rpsapp/', include('rpsapp.urls')),
+    path('polls/', include('polls.urls')),
 ]
