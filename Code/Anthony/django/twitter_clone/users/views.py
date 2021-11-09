842015ed48ac514a960d58c34819871b40d75ea9
Anthony Wallace <anthony@Anthonys-MBP.lan>
2021-11-08 20:56:42 -0800

    user app refactor

--- Code/Anthony/django/twitter_clone/users/views.py
+++ Code/Anthony/django/twitter_clone/users/views.py
@@ -1,20 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from django import forms
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
-class NewSignupForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=20)
-    password = forms.CharField(widget=forms.PasswordInput, label='Password', max_length=10)
-    first_name = forms.CharField(label='First Name', max_length=20)
-    last_name = forms.CharField(label='Last Name', max_length=20)
-    email = forms.EmailField(label='Email')
-
-class NewLoginForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=20)
-    password = forms.CharField(widget=forms.PasswordInput, label='Password', max_length=10)
+from .forms import NewLoginForm, NewSignupForm
 
 
 # Create your views here.
