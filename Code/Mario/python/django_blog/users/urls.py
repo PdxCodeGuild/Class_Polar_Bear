b999291908fa265cbd479d33ec8def9c95684a1e
Mario Gomes <mario.mgomes1@gmail.com>
2021-11-15 23:36:07 -0500

    BlogPost models

--- Code/Mario/python/django_blog/users/urls.py
+++ Code/Mario/python/django_blog/users/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.register, name='register'),
-    path('login', views.login, name='login')
+    path('login', views.user_login, name='user_login'),
+    path('welcome', views.welcome, name='welcome'),
+
 ]
