1b4e01ba405b4b885885b5fe84c905510c0eee42
gabinochacon8 <gabino.chacon8@gmail.com>
2021-11-18 21:45:08 -0800

    Deleted mobs lab

--- Code/Gabe/django/mobs/library/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-  path('', views.index, name='index'),
-  # path('checkin/', views.checkin, name='checkin'),
-  # path('checkout/', views.checkout, name='checkout'),
-]
