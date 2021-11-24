9c725801210bfa682832d9a4f02d07f6bc9fc764
dj51234 <dj51234@yahoo.com>
2021-11-24 17:22:06 -0500

    v1 and v2 of js redo lab

--- Code/Derrick/JavaScript/labs/javascript redo/V2/main.js
+++ Code/Derrick/JavaScript/labs/javascript redo/V2/main.js
@@ -28,10 +28,10 @@ function reset() {
 
 function randomize(r) { 
     for (let i = r.length - 1; i > 0; i--) {
-        let randI = Math.floor(Math.random() * (i + 1));
+        let randI = Math.floor(Math.random() * (i + 1)); // Semicolon needed because next line starts with [
 
         // swaps 2 letters, random letter from array (randI) and letter at current index (i) to shuffle password 
-        [r[i], r[randI]] = [r[randI], r[i]];
+        [r[i], r[randI]] = [r[randI], r[i]]
     }
     return r.join('')
 }
