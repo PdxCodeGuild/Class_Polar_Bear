2f2c81d2bdf30469f544b1268820d25a4e6c28dc
jamest7 <james.ed.thornton@gmail.com>
2021-11-24 23:35:04 -0600

    grocery list

--- Code/James/django/labs/grocery_list/urls.py
+++ Code/James/django/labs/grocery_list/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 app_name = 'grocery_list'
 urlpatterns = [
-    path('grocery_list/', views.grocery_list, name='grocery_list'),
+    path('', views.grocery_list, name='grocery_list'),
     path('grocery_list/', views.input, name='input'),
-    path('grocery_list/', views.listprint, name='listprint'),
     path('buy/<int:item_id>/', views.buy_item, name='buy'),
     path('delete/<int:item_id>/', views.delete_item, name='delete'),
 ]
