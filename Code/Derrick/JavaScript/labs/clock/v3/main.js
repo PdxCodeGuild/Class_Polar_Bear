817748fa4fc6497e65e17f2a25b00e2e5a7909a8
dj51234 <dj51234@yahoo.com>
2021-12-03 23:27:05 -0500

    js clock v1-v2 both using Date()

---
+++ Code/Derrick/JavaScript/labs/clock/v3/main.js
@@ -0,0 +1,13 @@
+const $startTime = document.querySelector("#start-time");
+const $addTime = document.querySelector("#time");
+const $milliseconds = document.querySelector("#milli");
+const $seconds = document.querySelector("#sec");
+const $minutes = document.querySelector("#min");
+
+$startTime.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  // convert to milliseconds
+  let time = $addTime.value * 60000;
+  console.log(time);
+});
