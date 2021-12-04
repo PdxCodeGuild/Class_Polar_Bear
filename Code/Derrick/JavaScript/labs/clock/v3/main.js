29b8c3c2dd7d513d7976f2812683c20c658150b6
dj51234 <dj51234@yahoo.com>
2021-12-04 00:39:49 -0500

    v3 clock using Date()

--- Code/Derrick/JavaScript/labs/clock/v3/main.js
+++ Code/Derrick/JavaScript/labs/clock/v3/main.js
@@ -6,9 +6,44 @@ const $minutes = document.querySelector("#min");
 
 $startTime.addEventListener("click", (event) => {
   event.preventDefault();
+  let start = new Date();
 
-  // convert to milliseconds
-  let time = $addTime.value * 60000;
-  
-  console.log(time);
+  let countdown = setInterval(() => {
+    let end = new Date();
+    let inputInMilliseconds = $addTime.value * 60000;
+    let diff = end - start;
+    let result = inputInMilliseconds - diff;
+    let milliseconds = Math.floor(Math.round(diff)).toString();
+    milliseconds = parseInt(milliseconds.slice(-2));
+    let seconds = Math.floor(result / 1000) % 60;
+    let minutes = Math.floor(result / 60000) % 60;
+
+    if (inputInMilliseconds - diff === 0) {
+      alert("hit zero");
+    }
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+
+    $milliseconds.textContent = milliseconds;
+    $seconds.textContent = seconds;
+    $minutes.textContent = minutes;
+
+    if ((milliseconds <= 0) & (seconds <= 0) & (minutes <= 0)) {
+      clearInterval(countdown);
+      milliseconds = 0;
+      seconds = 0;
+      minutes = 0;
+      $milliseconds.textContent = `00`;
+      $seconds.textContent = `00`;
+      $minutes.textContent = `00`;
+      alert("You hit 0!");
+    }
+  }, 10);
 });
+
+// milliseconds = Math.floor(Math.round(end - start)).toString();
+// milliseconds = parseInt(milliseconds.slice(-2));
