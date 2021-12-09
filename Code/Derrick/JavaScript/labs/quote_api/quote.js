7fc1f3f6128c21de049d75a80779af54dacbbb4a
dj51234 <dj51234@yahoo.com>
2021-12-08 23:17:36 -0500

    random quote generator lab

--- Code/Derrick/JavaScript/labs/quote_api/quote.js
+++ Code/Derrick/JavaScript/labs/quote_api/quote.js
@@ -2,17 +2,13 @@ const $quoteAuthor = document.querySelector('#quote-author')
 const $quoteBody = document.querySelector('#quote-body')
 const $newQuoteBtn = document.querySelector('#new-quote-btn')
 
-function newQuote() {
-    
-}
-
 $newQuoteBtn.addEventListener('click',function(){
     axios({
         method: 'get',
         url: 'https://favqs.com/api/qotd'
     }).then(response => {
-        let author = response.data.quote.author
-        let body = response.data.quote.body
+        const author = response.data.quote.author
+        const body = response.data.quote.body
         $quoteAuthor.textContent = `- ${author}`
         $quoteBody.textContent = body
     })
