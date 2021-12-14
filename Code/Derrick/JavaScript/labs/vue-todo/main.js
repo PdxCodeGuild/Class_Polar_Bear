095a5a7505c52cd5a6ea1d35c79e57859d6b715e
dj51234 <dj51234@yahoo.com>
2021-12-14 00:16:44 -0500

    vue todo lab

---
+++ Code/Derrick/JavaScript/labs/vue-todo/main.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    todos: [],
+    todoToAdd: "",
+  },
+  methods: {
+    getTodo: function () {
+      this.todos.push(this.todoToAdd);
+      this.todoToAdd = null;
+    },
+    completeTodo: function (i, event) {
+      const todoText = event.target.closest(".todo-item").firstChild.firstChild;
+
+      if (todoText.style.textDecoration === "line-through") {
+        todoText.style.textDecoration = "none";
+      } else {
+        todoText.style.textDecoration = "line-through";
+      }
+      console.log(todoText);
+    },
+    removeTodo: function (i) {
+      this.todos.splice(i, 1);
+    },
+  },
+});
