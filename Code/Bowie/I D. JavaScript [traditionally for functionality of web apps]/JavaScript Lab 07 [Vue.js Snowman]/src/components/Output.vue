d5301f405b29ff09a18f09cb853a59a41fc1a2fc
dannypacific <90663394+dannypacific@users.noreply.github.com>
2021-12-21 19:58:14 +0200

    snowman lab and Python update removing trivial words from word count

---
+++ Code/Bowie/I D. JavaScript [traditionally for functionality of web apps]/JavaScript Lab 07 [Vue.js Snowman]/src/components/Output.vue
@@ -0,0 +1,12 @@
+<template>
+  <h3>You have / Vous avez <span class="text-danger">{{count}}</span> attempts </h3>
+</template>
+
+<script>
+export default {
+  name: 'Output',
+  props: {
+    count: Number
+  }
+}
+</script>
