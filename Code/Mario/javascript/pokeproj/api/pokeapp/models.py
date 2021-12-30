2672f804626f7382c3b99ec8957afe7ce4b2596a
Mario Gomes <mario.mgomes1@gmail.com>
2021-12-29 22:14:59 -0500

    new changes

---
+++ Code/Mario/javascript/pokeproj/api/pokeapp/models.py
@@ -0,0 +1,13 @@
+# from django.db import models
+
+# # Create your models here.
+
+
+# class Pokemon(models.Model):
+#     number = models.IntegerField(_(""))
+#     name = models.CharField(_(""), max_length=50)
+#     height = models.FloatField(_(""))
+#     weight = models.FloatField(_(""))
+#     image_front = models.CharField(_(""), max_length=50)
+#     image_back = models.CharField(_(""), max_length=50)
+#     types = models.CharField(_(""), max_length=50)
