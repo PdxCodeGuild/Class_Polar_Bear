f36a0c2a7772c0397caec1d9778eabbce3a295e8
jamest7 <james.ed.thornton@gmail.com>
2022-01-21 19:09:24 -0600

    Capstone Update

---
+++ Code/James/capstone/Feature Backlog.md
@@ -0,0 +1,24 @@
+# Capstone Backlog
+
+## Vue Forms
+- Manufacturer 
+- Customer
+- Delete Buttons
+- Edit Buttons
+
+## Javascript
+- Manufacturer 
+- Customer
+
+## Python
+- Logic to calculate scores
+- Logic for downstream effects
+
+# SQL
+- Change form SQLite to SQL
+
+## Exception Handling
+- Show points on map w/o data
+
+## Bugs
+- Manufacturer Submit Button
--- Code/James/capstone/cap/valuechain/static/valuechain/main.js
+++ Code/James/capstone/cap/valuechain/static/valuechain/main.js
@@ -79,13 +79,7 @@ function initMap(supplierdata) {
     // First layer is JSON, second layer is an object
     console.log(supplierdata[0].capacity)
     let content1 = '<h2>Manufacturer</h2>'
-    let imageRed = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
-    let imageGreen = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
-    let imageAmber = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
-    // if (suppliderdata[0].capacity <5
-*/
-    
-    
+    */
 
     // Map options (used https://youtu.be/Zxf1mnP5zcw Tutorial)
     var options = {
@@ -113,29 +107,70 @@ function initMap(supplierdata) {
     marker.addListener('click', function(){
         infoWindow.open(map,marker);
     });
-
     */
+
+
+    let imageRed = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+    let imageGreen = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+    let imageAmber = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
+    
+    // var colorManufacturer = imageGreen // Check
+    let z = supplierdata[0].capacity/2 + supplierdata[1].capacity/2
+    if (z < 3) {
+        var colorManufacturer = imageRed
+    } else if (z < 8) {
+        var colorManufacturer = imageAmber
+    } else if (z < 11) {
+        var colorManufacturer = imageGreen
+    } else {
+        var colorManufacturer = imageGreen
+    }
+
+    // var colorS1 = imageGreen // Check
+    let a = supplierdata[0].capacity
+    if (a < 3) {
+        var colorS1 = imageRed
+    } else if (a < 8) {
+        var colorS1 = imageAmber
+    } else if (a < 11) {
+        var colorS1 = imageGreen
+    } else {
+        var colorS1 = imageGreen
+    }
+
+    // var colorS2 = imageGreen // Check
+    let b = supplierdata[1].capacity
+    if (b < 3) {
+        var colorS2 = imageRed
+    } else if (b < 8) {
+        var colorS2 = imageAmber
+    } else if (b < 11) {
+        var colorS2 = imageGreen
+    } else {
+        var colorS2 = imageGreen
+    }
+    
 
     // Array of markers (taken out: content: content1,)
     var markers = [
         {
             coords:{lat:30.2672,lng:-97.7491},
-            iconImage:'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+            iconImage:colorManufacturer, //'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
             content: '<h2>Manufacturer</h2>',
         },
         {
             coords:{lat:29.4241,lng:-98.4936},
-            iconImage:'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
+            iconImage:colorS1, //'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
             content:'<h2>Supplier 1</h2>',
         },
         {
             coords:{lat:29.7066,lng:-96.5397},
-            iconImage:'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+            iconImage:'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
             content:'<h2>Customer</h2>',
         },
         {
             coords:{lat:31.0982,lng:-97.3428},
-            iconImage:'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+            iconImage:colorS2, //'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
             content:'<h2>Supplier 2</h2>',
         },
     ];
@@ -205,9 +240,8 @@ function initMap(supplierdata) {
     let colorRed = "#FF0000"
     let colorBlack = "000000"
 
-
-    // colorSanAusPath = '#FF0000'
-    let x = supplierdata[0].capacity
+    /colorSanAusPath
+    let x = supplierdata[0].timeliness
     if (x < 3) {
         var colorSanAusPath = colorRed
     } else if (x < 8) {
@@ -217,6 +251,19 @@ function initMap(supplierdata) {
     } else {
         var colorSanAusPath = colorBlack
     }
+
+    // colorTemAustPath
+    let y = supplierdata[1].timeliness
+    if (y < 3) {
+        var colorTemAusPath = colorRed
+    } else if (y < 8) {
+        var colorTemAusPath = colorYellow
+    } else if (y < 11) {
+        var colorTemAusPath = colorGreen
+    } else {
+        var colorTemAusPath = colorBlack
+    }
+    
     
 
     // Add Paths         strokeColor: colorSanAusPath,
@@ -230,14 +277,14 @@ function initMap(supplierdata) {
     const temAusPath = new google.maps.Polyline({
         path: temAusCoordinates,
         geodesic: true,
-        strokeColor: "#33FF36",
+        strokeColor: colorTemAusPath, //"#F0FF33",
         strokeOpacity: 1.0,
         strokeWeight: 6,
       });
     const ausColPath = new google.maps.Polyline({
         path: ausColCoordinates,
         geodesic: true,
-        strokeColor: "#FF0000",
+        strokeColor: "#F0FF33",
         strokeOpacity: 1.0,
         strokeWeight: 6,
         });
